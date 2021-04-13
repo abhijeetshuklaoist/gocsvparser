99f2b9f79b41754df8deb160dd7049399d9fd147
ABHIJEET SHUKLA <abhijeet.shukla@sap.com>
2021-04-13 13:59:39 +1000

    Tested with 2M rows

--- awesomeProject/csvparser/CSVParser.go
+++ awesomeProject/csvparser/CSVParser.go
@@ -1,11 +1,18 @@
 package main
 
-import "csvpaser/parser"
+import (
+	"csvpaser/parser"
+	"log"
+	"time"
+)
 
 func main() {
+	start := time.Now()
 	csvPath := "csvs/data/rostertest.csv"
 	outputCSVPath := "csvs/output/"
 	mappingCSVPath := "utils/mappings.csv"
 	parser.ProcessCSV(csvPath, outputCSVPath, mappingCSVPath)
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed)
 }
 
--- awesomeProject/csvparser/CreateTestData.go
+++ awesomeProject/csvparser/CreateTestData.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"csvpaser/utils"
+	"log"
 	"strconv"
+	"time"
 )
 
 func createData()  {
+	start := time.Now()
 	csvPathString := "csvs/data/rostertest.csv"
-	var data = []string {"Name" , "Email", "Salary", "ID"}
+	var data = []string {"Name" , "Email", "ExtraColumn1", "ExtraColumn2", "ExtraColumn3", "Salary", "ID", "ExtraColumn4",
+		"ExtraColumn5", "ExtraColumn6", "ExtraColumn7", "ExtraColumn8", "ExtraColumn9"}
 	utils.WriteDataInCSVSync(csvPathString, data)
-	for i:= 1; i < 1000000; i++ {
+	for i:= 1; i < 1000000; i ++ {
 		var data = []string {"Name" + strconv.Itoa(i), "EmailSomething@gmai.com"+ strconv.Itoa(i),
-			"Salary" + strconv.Itoa(i), "ID" + strconv.Itoa(i)}
+			"ExtraColumn1" +  strconv.Itoa(i), "ExtraColumn2" +  strconv.Itoa(i), "ExtraColumn3" +  strconv.Itoa(i),
+			"Salary" + strconv.Itoa(i), "ID" + strconv.Itoa(i),
+			"ExtraColumn4" +  strconv.Itoa(i), "ExtraColumn5" +  strconv.Itoa(i), "ExtraColumn6" +  strconv.Itoa(i),
+			"ExtraColumn7" +  strconv.Itoa(i), "ExtraColumn8" +  strconv.Itoa(i), "ExtraColumn9" +  strconv.Itoa(i)}
 		utils.WriteDataInCSVSync(csvPathString, data)
-	}
 
+		var wrongdata = []string {"Name" + strconv.Itoa(i), "",
+			"ExtraColumn1" +  strconv.Itoa(i), "ExtraColumn2" +  strconv.Itoa(i), "ExtraColumn3" +  strconv.Itoa(i),
+			"Salary" + strconv.Itoa(i), "ID" + strconv.Itoa(i),
+			"ExtraColumn4" +  strconv.Itoa(i), "ExtraColumn5" +  strconv.Itoa(i), "ExtraColumn6" +  strconv.Itoa(i),
+			"ExtraColumn7" +  strconv.Itoa(i), "ExtraColumn8" +  strconv.Itoa(i), "ExtraColumn9" +  strconv.Itoa(i)}
+		utils.WriteDataInCSVSync(csvPathString, wrongdata)
+	}
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed)
 }
 
 func main1() {
